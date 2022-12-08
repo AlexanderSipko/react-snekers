20ce93c76215f2dcedc3435dff91e6d821d3456a
Asipko <avsipko@gmail.com>
2022-12-08 21:00:58 +0500

    font-body

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
 
   return (
     <div className="wrapper clear">
-      <header className="d-flex justify-between">
+      <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.svg" alt='logo' />
           <div>
-            <h3>React snekers</h3>
+            <h3 className="text-uppercase">React snekers</h3>
             <p>Магазин лучших кросовок</p>
           </div>
         </div>
         <ul className="d-flex">
-          <li>
+          <li className="mr-30">
           <img width={18} height={18} src="/img/heart.svg" alt='heart' />
             <span>1205 руб.</span>
           </li>
@@ -24,7 +24,7 @@ function App() {
           </li>
         </ul>
       </header>
-      <div className="content">
+      <div className="content p-40">
         <h1>Все кроссовки</h1>
         ...
       </div>
