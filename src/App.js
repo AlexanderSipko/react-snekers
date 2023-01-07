522080a6d2aade0e6a98df6ec1dc08fd937d33bd
Asipko <avsipko@gmail.com>
2023-01-07 20:53:10 +0500

    correct items new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import axios from "axios";
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 // import Card from "./components/Card";
 
 import Header from "./components/Header";
